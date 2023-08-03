7dc0d789feafb7b3e973c1a8bfd89b9ca4ed3220
Iamneo <techadmin@iamneo.ai>
2023-08-03 12:18:18 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -18,7 +18,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
